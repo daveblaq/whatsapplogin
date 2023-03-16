813a8ee3496a0b06c5978f387bb4f742819e3dc0
daveblaq <davidoladele80@gmail.com>
2023-03-16 15:17:50 +0100

    Fixes

--- src/App.js
+++ src/App.js
@@ -11,14 +11,18 @@ function App() {
       const waNumber = otplessUser.waNumber;
 
       // Handle the signup/signin process
-      // ...
+      // 
     };
   }, []);
   return (
-    <div className="App">
-      <h3>Whatsapp OTP Login</h3>
-    
-      <div id="otpless"></div>
+    <div className="App ">
+      <div className="container mx-auto md:w-full w-11/12 flex items-center justify-center ">
+        <div className="bg-white shadow-lg flex items-center flex-col justify-center w-[40%] rounded-xl py-[50px] absolute top-[20%]">
+          <h3 className="pb-8">Whatsapp OTP Login</h3>
+
+          <div id="otpless"></div>
+        </div>
+      </div>
     </div>
   );
 }
